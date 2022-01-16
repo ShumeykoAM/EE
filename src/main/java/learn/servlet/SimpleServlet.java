35d1ff5f70b6a30798a5f1f4b17567f0f0d238af
kot <shumeyko.a.m@gmail.com>
2022-01-16 18:51:41 +0300

    Создал проект Java 8 EE для СП GlassFish 5
    Реализовал пример пользовательского сервлета
    Запуск сервлета сконфигурированного в web.xml
        http://localhost:8080/GlassFishApp/servlet/webXmlSimpleServlet
    Запуск сервлета сконфигурированного через аннотацию
        http://localhost:8080/GlassFishApp/servlet/annotatedSimpleServlet

---
+++ src/main/java/learn/servlet/SimpleServlet.java
@@ -0,0 +1,51 @@
+package learn.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/annotatedSimpleServlet")
+public class SimpleServlet extends HttpServlet
+{
+	private String message;
+	private static final Cookie[] COOKIES_EMPTY_ARRAY = new Cookie[0];
+
+	@Override
+	public void init() throws ServletException
+	{
+		message = "first";
+	}
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		Integer count = 1;
+		Cookie[] cookies = request.getCookies();
+		for (Cookie cookie : (cookies != null ? cookies : COOKIES_EMPTY_ARRAY))
+			if ("counter".equals(cookie.getName()))
+				count = Integer.valueOf(cookie.getValue()) + 1;
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>" + String.format("Hello, it is your %s visit.", count) + "</h1>");
+
+		//Создаем куку
+		Cookie cookie = new Cookie("counter", count.toString());
+		//Максимальное время жизни куки
+		cookie.setMaxAge(5);
+
+		//Добавляем куку в ответ, браузер сохранит ее у себя
+		response.addCookie(cookie);
+	}
+
+	@Override
+	public void destroy()
+	{
+		message = null;
+	}
+}
